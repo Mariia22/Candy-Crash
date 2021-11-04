0fcd9cbab47d9f3737fc70b9c1ccf236e348c235
Maria22 <m.ushakova22@gmail.com>
2021-11-04 13:57:16 +0100

    Add functions to delete columns/rows after loading board

--- notes/src/App.js
+++ notes/src/App.js
@@ -4,24 +4,80 @@ import { GlobalStyle, lightTheme, darkTheme } from './GlobalStyle';
 import { WrapperStyle, GameStyle, GameImg } from './WrapperStyle';
 import ToggleButton from './components/ToggleButton/ToggleButton';
 
+const width = 8;
+
 function App() {
   const [theme, setTheme] = useState(false);
-  const [board, setBoard] = useState([]);
+  const [boardArray, setBoard] = useState([]);
 
 
   function fillBoard() {
-    const width = 8;
-    const boardArray = [];
+    const board = [];
     const colors = ['red', 'yellow', 'blue', 'purple', 'orange', 'green'];
     for (let i = 0; i < width * width; i++) {
-      boardArray.push(colors[Math.floor(Math.random() * colors.length)]);
+      board.push(colors[Math.floor(Math.random() * colors.length)]);
     }
-    setBoard(boardArray)
+    setBoard(board)
+  }
+
+  function deleteFourBlocksOnRow() {
+    for (let i = 0; i < boardArray.length; i++) {
+      const optionalArray = [i, i + 1, i + 2, i + 3];
+      let defineColor = boardArray[i];
+      const isExeption = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63];
+      if (isExeption.includes(i)) continue;
+      if (optionalArray.every(index => boardArray[index] === defineColor)) {
+        optionalArray.forEach(index => boardArray[index] = '');
+      }
+    }
+  }
+
+  function deleteThreeBlocksOnRow() {
+    for (let i = 0; i < boardArray.length; i++) {
+      const optionalArray = [i, i + 1, i + 2];
+      let defineColor = boardArray[i];
+      const isExeption = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
+      if (isExeption.includes(i)) continue;
+      if (optionalArray.every(index => boardArray[index] === defineColor)) {
+        optionalArray.forEach(index => boardArray[index] = '');
+      }
+    }
+  }
+
+  function deleteFourBlocksOnColumns() {
+    for (let i = 0; i < 47; i++) {
+      const optionalArray = [i, i + width, i + 2 * width, i + 3 * width];
+      let defineColor = boardArray[i];
+      if (optionalArray.every(index => boardArray[index] === defineColor)) {
+        optionalArray.forEach(index => boardArray[index] = '');
+      }
+    }
+  }
+
+  function deleteThreeBlocksOnColumns() {
+    for (let i = 0; i < 53; i++) {
+      const optionalArray = [i, i + width, i + 2 * width];
+      let defineColor = boardArray[i];
+      if (optionalArray.every(index => boardArray[index] === defineColor)) {
+        optionalArray.forEach(index => boardArray[index] = '');
+      }
+    }
   }
 
   useEffect(() => {
     fillBoard()
   }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      deleteFourBlocksOnRow();
+      deleteFourBlocksOnColumns();
+      deleteThreeBlocksOnRow();
+      deleteThreeBlocksOnColumns()
+      setBoard([...boardArray]);
+    }, 100)
+    return () => clearInterval(timer);
+  }, [deleteFourBlocksOnRow, deleteFourBlocksOnColumns, deleteThreeBlocksOnRow, deleteThreeBlocksOnColumns, boardArray])
 
   return (
     <ThemeProvider theme={theme === false ? lightTheme : darkTheme}>
@@ -30,7 +86,7 @@ function App() {
         <ToggleButton onChange={(event) => setTheme(event.target.checked)} />
         <h1>Candy Crash</h1>
         <GameStyle>
-          {board.map((color, index) =>
+          {boardArray.map((color, index) =>
             <GameImg style={{ backgroundColor: color }} key={index} />)}
         </GameStyle>
       </WrapperStyle>
